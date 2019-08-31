a8570fe4783cb7f2958ec6b8d0adb8a6a39c35a7
jordan-fox <46389457+jordan-fox@users.noreply.github.com>
2019-08-30 19:59:58 -0700

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,1 +1,14 @@
 prompt('How\'s it goin\'')
+
+function askName() {
+    var name = prompt('What\'s your name?');
+    var output = 'Hello, ' + name + '!';
+    var answer = confirm('Do you like books AND movies?');
+
+    if (answer) {
+        output = output + ' Get Excited!';
+    } else {
+        output = output + ' Get out!';
+    }  
+
+return output; 
